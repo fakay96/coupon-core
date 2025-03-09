9d428c024940b1120cb49b2330331ad58e7278aa
Gabriel <onlinehassle1234@gmail.com>
2025-03-09 16:48:17 +0100

    updated the vit configuration file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,15 +9,10 @@ dotenv.config();
 
 // Check if the "./env" directory exists before trying to load
 const envDirPath = path.resolve(__dirname, './env');
-const env = fs.existsSync(envDirPath) ? loadEnv(process.env.NODE_ENV, envDirPath) : process.env;
+const env = fs.existsSync(envDirPath) ? loadEnv(process.env.NODE_ENV as string, envDirPath) : process.env;
 
 export default defineConfig({
   plugins: [react()], // Enables React support
-<<<<<<< HEAD
-  envDir: './env', // Specify the directory containing your .env files]
-  server: {
-  host: true, 
-=======
   envDir: './env', // Specify the directory containing your .env files
 
   define: {
@@ -25,14 +20,7 @@ export default defineConfig({
   },
 
   server: {
-    host: true, // Allows network access to the dev server
-    // proxy: {
-    //   "/api": {
-    //     target: "https://api.dishpal.ai", // Redirects API requests to the backend
-    //     changeOrigin: true, // Modifies the origin header for CORS handling
-    //   },
-    // },
->>>>>>> 8b414ce535de17477594e9650c1c948ba11f243d
+    host: true, 
   },
 
   resolve: {
@@ -40,17 +28,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"), // Creates a shorthand for importing from "src"
     },
   },
-<<<<<<< HEAD
 });
-
-
-// Allows network access to the dev server
-  // proxy: {
-  //   "/api": {
-  //     target: "https://api.dishpal.ai", // Redirects API requests to the backend
-  //     changeOrigin: true, // Modifies the origin header for CORS handling
-  //   },
-  // },
-=======
-});
->>>>>>> 8b414ce535de17477594e9650c1c948ba11f243d
