c7cdf91f480b8c9c5cf8bc107195873d4ad9582d
Erayolu <ireoluwa.fakeye2022@my.ntu.ac.uk>
2025-05-25 17:12:40 +0100

    Refactor DiscountPage component and update DiscountItem type
    - Removed unused imports and properties from DiscountItem type.
    - Updated the DiscountPage component to use 'title' instead of 'id' for keys and adjusted displayed values for price and original price.
    - Ensured navigate function is included in the dependency array of useEffect for proper functionality.

--- coupon_core/settings/staging.py
+++ coupon_core/settings/staging.py
@@ -167,7 +167,7 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://app-staging.dishpal.ai",
-    "https://admin-staging.dishpal.ai"
+    "https://admin-staging.dishpal.ai",
     "http://0.0.0.0:5173",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
